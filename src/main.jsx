79c11f1495cdf716becfb14a0a4f71d8325b226b
harshpathakzz <harshpathak2017@gmail.com>
2023-05-31 10:42:18 +0530

    added theme to app

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CartProvider from "./context/CartContext";
 import "./index.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <ThemeProvider theme={theme}>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
