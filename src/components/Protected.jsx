449f1e14c5bd3638391f1179c7d8f1179c1745c1
harshpathakzz <harshpathak2017@gmail.com>
2023-06-13 19:38:48 +0530

    updated

--- src/components/Protected.jsx
+++ src/components/Protected.jsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const Protected = ({ isLoggedIn, children }) => {
   if (!isLoggedIn) {
-    return <Navigate to="/login" />;
+    return <Navigate to="/request-login" />;
   }
   return children;
 };
