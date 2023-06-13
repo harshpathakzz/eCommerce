fd6f77f62c7d664a7dbdcf8cea201d918193d9ce
harshpathakzz <harshpathak2017@gmail.com>
2023-06-13 19:37:49 +0530

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,32 @@ import CartPage from "./pages/CartPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { useUserAuth } from "./context/UserAuthContext";
+import Protected from "./components/Protected";
+import RequestLoginPage from "./pages/RequestLoginPage";
 
 function App() {
+  const { isLoggedIn } = useUserAuth();
   return (
     <>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
 
-          <Route path="/cart" element={<CartPage />} />
+          <Route
+            path="/cart"
+            element={
+              <Protected isLoggedIn={isLoggedIn}>
+                <CartPage />
+              </Protected>
+            }
+          />
 
           <Route path="/login" element={<LoginPage />} />
 
           <Route path="/signup" element={<SignUpPage />} />
+
+          <Route path="/request-login" element={<RequestLoginPage />} />
 
           <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
         </Routes>
