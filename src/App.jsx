8ee7e23131915d272162165715663d711c49aa07
harshpathakzz <harshpathak2017@gmail.com>
2023-06-13 18:25:03 +0530

    added new routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
@@ -11,6 +13,10 @@ function App() {
           <Route path="/" element={<HomePage />} />
 
           <Route path="/cart" element={<CartPage />} />
+
+          <Route path="/login" element={<LoginPage />} />
+
+          <Route path="/signup" element={<SignUpPage />} />
 
           <Route path="*" element={<h1> PAGE NOT FOUND</h1>} />
         </Routes>
