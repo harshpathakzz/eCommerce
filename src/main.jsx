ae7b38f368418b9544990355ed4d92c06ec1ef12
harshpathakzz <harshpathak2017@gmail.com>
2023-06-14 17:51:24 +0530

    added SearchProvider

--- src/main.jsx
+++ src/main.jsx
@@ -8,16 +8,18 @@ import { theme } from "./theme";
 import CssBaseline from "@mui/material/CssBaseline";
 import { UserAuthProvider } from "./context/UserAuthContext.jsx";
 import { FilterProvider } from "./context/FilterContext";
-
+import { SearchProvider } from "./context/SearchContext";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <UserAuthProvider>
         <CartProvider>
-          <FilterProvider>
-            <App />
-          </FilterProvider>
+          <SearchProvider>
+            <FilterProvider>
+              <App />
+            </FilterProvider>
+          </SearchProvider>
         </CartProvider>
       </UserAuthProvider>
     </ThemeProvider>
